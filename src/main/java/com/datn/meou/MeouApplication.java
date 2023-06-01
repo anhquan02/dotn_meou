45a893d8f63031ae8d9d887b46a895d058caaa4b
anhquan02 <quan.leanh.02@gmail.com>
2023-06-02 00:14:06 +0700

    init project

---
+++ src/main/java/com/datn/meou/MeouApplication.java
@@ -0,0 +1,13 @@
+package com.datn.meou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeouApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/datn/meou/MeouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datn.meou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

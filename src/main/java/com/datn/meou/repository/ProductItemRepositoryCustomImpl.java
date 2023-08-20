ca0dae9fc692692bb48fa69c87e5f25d7992475d
namen1706 <namen1706@gmail.com>
2023-08-20 13:46:04 +0700

    update search

--- src/main/java/com/datn/meou/repository/ProductItemRepositoryCustomImpl.java
+++ src/main/java/com/datn/meou/repository/ProductItemRepositoryCustomImpl.java
@@ -29,7 +29,7 @@ public class ProductItemRepositoryCustomImpl implements ProductItemRepositoryCus
                 " JOIN dotn_insole ins ON ins.id = pi.insole_id" +
                 " JOIN dotn_size sz ON sz.id = pi.size_id" +
                 " JOIN dotn_color c ON c.id = pi.color_id" +
-                " JOIN dotn_brand b ON b.id = p.brand_id" +
+                " JOIN dotn_brand b ON b.id = pi.brand_id" +
                 " WHERE pi.status = 1  ");
         Map<String, Object> params = new HashMap<>();
         if (!DataUtil.isNullObject(dto.getName())) {

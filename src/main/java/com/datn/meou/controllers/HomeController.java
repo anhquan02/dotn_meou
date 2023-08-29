722356998e93bbb92c2ab7c2b4926f4375647d41
luong08012002 <94169677+luong08012002@users.noreply.github.com>
2023-08-30 01:07:31 +0700

    commit

--- src/main/java/com/datn/meou/controllers/HomeController.java
+++ src/main/java/com/datn/meou/controllers/HomeController.java
@@ -3,24 +3,21 @@ package com.datn.meou.controllers;
 import com.datn.meou.model.ProductDTO;
 import com.datn.meou.repository.ProductRepository;
 import com.datn.meou.util.ResponseUtil;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
-@RequestMapping("/api/v1/customer")
+@RequestMapping("/api/v1/home")
 @RequiredArgsConstructor
-public class CustomerManagerController {
+public class HomeController {
 
     private final ProductRepository productRepository;
 
-    @PostMapping("home")
-    private ResponseEntity<?> advancedSearchProduct(@RequestBody ProductDTO dto, Pageable pageable) {
+    @PostMapping()
+    private ResponseEntity<?> home(@RequestBody ProductDTO dto, Pageable pageable) {
         return ResponseUtil.ok(this.productRepository.advancedSearchPage(dto, pageable));
     }
 }

9a79bae1f49ba9ea88c5deb737d485dc3f57d7dc
namen1706 <namen1706@gmail.com>
2023-09-09 14:52:45 +0700

    delete cart cart item

--- src/main/java/com/datn/meou/repository/CartItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.datn.meou.repository;
-
-import com.datn.meou.entity.CartItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CartItemRepository extends JpaRepository<CartItem, Long> {
-}

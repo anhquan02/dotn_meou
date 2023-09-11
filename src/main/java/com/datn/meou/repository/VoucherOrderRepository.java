71465a90face6278f4442d512f270e1fcd02c476
namen1706 <namen1706@gmail.com>
2023-09-11 20:21:17 +0700

    Commit voucher

--- src/main/java/com/datn/meou/repository/VoucherOrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.datn.meou.repository;
-
-import com.datn.meou.entity.VoucherOrder;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VoucherOrderRepository extends JpaRepository<VoucherOrder, Long> {
-}
